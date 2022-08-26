b46d5e56f0da239ae8bfdde1413b8dbb96d90a48
leeyeonji28 <yeon102800@gmail.com>
2022-08-26 22:43:56 +0900

    초기화

--- src/App.js
+++ src/App.js
@@ -1,52 +1,5 @@
-import { useState } from "react";
-import Convert from "./Convert";
-import Counter from "./Counter";
-
 function App() {
-  const [minutes, setMinutes] = useState(0);
-  const [disabled, setDisabled] = useState(false);
-
-  const onChange = (e) => {
-    setMinutes(e.target.value);
-  };
-  const changeActive = () => {
-    setDisabled(!disabled);
-  };
-  const reset = () => {
-    setMinutes(0);
-  };
-
-  return (
-    <div className="p-10">
-      <div className="font-bold text-2xl">Time Converter</div>
-      <div className="border border-pink-300 mb-3 mt-5">
-        <div>Minutes</div>
-        <input
-          type="number"
-          placeholder="Minutes"
-          value={minutes}
-          onChange={onChange}
-          disabled={disabled}
-        />
-      </div>
-      <div className="border border-blue-300 mb-5">
-        <div>Hours</div>
-        <input
-          type="number"
-          placeholder="Hours"
-          value={Math.floor(minutes / 60)}
-          onChange={() => {}}
-          disabled={!disabled}
-        />
-      </div>
-      <button onClick={changeActive} className="btn mr-3">
-        active toggle
-      </button>
-      <button onClick={reset} className="btn w-36">
-        reset
-      </button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/Convert.jsx
+++
@@ -1,5 +0,0 @@
-const Convert = ({counter, setCounter}) => {
-    return <div><div>{counter % 2 == 0 ? "짝수" : "홀수"}</div><button onClick={() => { setCounter(counter + 1)}}></button></div>
-}
-
-export default Convert;
