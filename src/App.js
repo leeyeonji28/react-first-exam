d19c714c803dfdf94678b9fbf3c306fee68edab7
leeyeonji28 <yeon102800@gmail.com>
2022-08-26 23:05:16 +0900

    useEffect 예제

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [counter, setCounter] = useState(0);
+  const [searchWord, serSearchWord] = useState("");
+  const inputChange = (e) => {
+    serSearchWord(e.target.value);
+  };
+  useEffect(() => {
+    console.log("API");
+  }, []);
+  useEffect(() => {
+    console.log("counter");
+  }, [counter]);
+  useEffect(() => {
+    console.log("searchWord");
+  }, [searchWord]);
+
+  return (
+    <div className="m-4">
+      <div>
+        <h1>검색어를 입력해주세요.</h1>
+        <input
+          type="text"
+          className="border-black border"
+          onChange={inputChange}
+        />
+      </div>
+
+      <div>
+        <h1>카운터</h1>
+        <p>{counter}번 입력했습니다.</p>
+        <button
+          className="btn"
+          onClick={() => {
+            setCounter(counter + 1);
+          }}
+        >
+          Click :)
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
